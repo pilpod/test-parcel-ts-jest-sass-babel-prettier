83d6d5324ee3f337035992d27d49c35d1005402b
pilpod <giacomo.corso@gmail.com>
2021-06-13 19:05:17 +0200

    grid finished

--- src/components/GridSection/GridComponent.ts
+++ src/components/GridSection/GridComponent.ts
@@ -17,11 +17,11 @@ export class GridComponent {
     {
         const grid = /* html */ `
             <div class="grid">
-                <div class="foto1"></div>
-                <div class="foto2"></div>
-                <div class="foto3"></div>
-                <div class="foto4"></div>
-                <div class="foto5"></div>
+                <div class="foto1"><img src="https://i.blogs.es/d93d8d/marvel/1366_2000.jpeg"></div>
+                <div class="foto3"><img src="https://as.com/meristation/imagenes/2021/04/26/noticias/1619428263_077178_1619428501_noticia_normal_recorte1.jpg"></div>
+                <div class="foto4"><img src="https://media.vandal.net/i/640x360/10-2020/2020102013243495_1.jpg"></div>
+                <div class="foto2"><img src="https://i1.wp.com/codigoespagueti.com/wp-content/uploads/2021/06/Estos-son-los-comics-que-inspiraron-la-nueva-obra-de-Marvel-Loki-1.png?fit=1280%2C720&quality=80&ssl=1"></div>
+                <div class="foto5"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRQ0wttOnMEfqS3PEMhemSSkE4x9WCO_MiYSA&usqp=CAU"></div>
             </div>
         `;
 
